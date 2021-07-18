2a6247153551341651d2baeb25fad665f3ea4304
Emily <36363495+em1lyy@users.noreply.github.com>
2021-07-18 23:35:07 +0200

    group sections in build script

--- build-docs.sh
+++ build-docs.sh
@@ -15,11 +15,25 @@ buildman() {
     pandoc -f markdown -t man -s $2/$1.md -o $2/$1
 }
 
+##
+# GENERAL COMMANDS
+##
+
 buildall cbsh.1 cbsh
 buildall nezumi.1 nezumi
+
+##
+# FILE FORMATS AND CONVENTIONS
+##
+
+buildall destlink.conf.5 destlink
+
+##
+# SYSTEM ADMINISTRATION COMMANDS AND DAEMONS
+##
+
 buildall deppa.8 deppa
 buildall destlink.8 destlink
-buildall destlink.conf.5 destlink
 
 # fix man pages
 # sed -i 's/.TH "CBSH(1)" "" "" "" ""/.TH "CBSH" "1" "cbsh-1.0" "December 2020" ""/' cbsh/cbsh.1
