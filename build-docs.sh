c66f76370c8fed803aae876acfb9699da897b2a8
Emily <36363495+em1lyy@users.noreply.github.com>
2021-07-18 22:44:11 +0200

    improve build system

--- build-docs.sh
+++ build-docs.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+buildall() {
+    buildman $1 $2
+    buildhtml $1 $2
+}
+
 buildhtml() {
     echo "Building html doc $1 in $2/..."
     pandoc -f markdown -t html -s $2/$1.md -o $2/$1.html
@@ -8,11 +13,11 @@ buildhtml() {
 buildman() {
     echo "Building man page $1 in $2/..."
     pandoc -f markdown -t man -s $2/$1.md -o $2/$1
-    buildhtml $1 $2
 }
 
-buildman cbsh.1 cbsh
-buildman nezumi.1 nezumi
+buildall cbsh.1 cbsh
+buildall nezumi.1 nezumi
+buildall deppa.1 deppa
 
 # fix man pages
 # sed -i 's/.TH "CBSH(1)" "" "" "" ""/.TH "CBSH" "1" "cbsh-1.0" "December 2020" ""/' cbsh/cbsh.1
